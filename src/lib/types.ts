dead182cbb2597258ceebe6914b266465021a449
invakid404 <invakid404@riseup.net>
2022-10-27 12:31:55 +0300

    feat(lib/types): implement `ToCamelCase`

---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+type CapitalizeFirst<Value extends string> = Capitalize<Lowercase<Value>>;
+
+type CamelCaseRec<Value extends string> =
+  Value extends `${infer First}_${infer Rest}`
+    ? `${CapitalizeFirst<First>}${CamelCaseRec<Rest>}`
+    : CapitalizeFirst<Value>;
+
+export type CamelCase<S extends string> = Uncapitalize<CamelCaseRec<S>>;
+
+export type ToCamelCase<T> = { [Key in keyof T as Key extends string ? CamelCase<Key> : never]: T[Key] }
