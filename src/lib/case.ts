dead52e9c54738b206f7c71af739a40aa78bb6fc
invakid404 <invakid404@riseup.net>
2022-10-27 12:32:12 +0300

    feat(lib/case): implement `toCamelCase`

---
+++ src/lib/case.ts
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+import {ToCamelCase} from './types'
+
+export type ToCamelCaseReturnValue<T> = T extends Array<infer E> ? ToCamelCase<E>[] : ToCamelCase<T>;
+
+export const toCamelCase = <T extends Record<string, unknown> | Record<string, unknown>[]>(value: T): ToCamelCaseReturnValue<T> =>  {
+  if (Array.isArray(value)) {
+    return value.map(element => toCamelCase(element)) as ToCamelCaseReturnValue<T>
+  }
+
+  return _.mapKeys(value as unknown as Record<string, unknown>, (_value, key) => _.camelCase(key)) as ToCamelCaseReturnValue<T>
+}
